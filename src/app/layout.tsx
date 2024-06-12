00176e81dcf08aeed05b3776e95a72fa25c84aa2
qbekkai <quentin.bekkai@gmail.com>
2024-06-12 09:02:02 +0200

    add all toast context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../style/globals.css';
+import { ToastContextProvider } from '@/contexts/toast.context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,12 +26,14 @@ export default function RootLayout({
 			className={`bg-slate-900 text-white w-[${h}px] h-[${w}px] overflow-hidden`}
 		>
 			<body className={inter.className + ' w-full h-full relative'}>
-				<header className="absolute p-2 w-full z-50">
-					<div className="bg-slate-800 w-full p-3 rounded-xl">
-						<h1>Home Storage Scanner</h1>
-					</div>
-				</header>
-				{children}
+				<ToastContextProvider>
+					<header className="absolute p-2 w-full z-50">
+						<div className="bg-slate-800 w-full p-3 rounded-xl">
+							<h1>Home Storage Scanner</h1>
+						</div>
+					</header>
+					{children}
+				</ToastContextProvider>
 			</body>
 		</html>
 	);
